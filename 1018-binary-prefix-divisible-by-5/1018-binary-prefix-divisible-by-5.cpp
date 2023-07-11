d294bc8db48dc3c92f00b3676c2b630f37151644
Faruk PEKTAS <97880185+farukdll@users.noreply.github.com>
2023-07-11 12:56:13 +0300

    Time: 7 ms (92.38%), Space: 13.9 MB (19.94%) - LeetHub

---
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> res;
+        int num;
+        
+        for(int i = 0; i < nums.size(); ++i){
+            num = (num * 2 + nums[i]) % 5;
+            res.push_back(num == 0);
+        }
+        return res;
+    }
+};
