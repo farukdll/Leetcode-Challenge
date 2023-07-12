3006e535226287d90cfd93b4f0ca218466f8029e
Faruk PEKTAS <97880185+farukdll@users.noreply.github.com>
2023-07-12 12:05:07 +0300

    Time: 16 ms (88.15%), Space: 17.9 MB (87.12%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       int x=0;
+       for(int l:nums){
+           x^=l;
+       }
+       for(int i=0;i<=nums.size();i++){
+           x^=i;
+       }
+       return x; 
+    }
+};
